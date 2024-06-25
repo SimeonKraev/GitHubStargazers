f89b75de7d15a5c509a156a55f772c9b15314757
SimeonKraev <shushumigaaa@gmail.com>
2024-06-25 13:05:27 +0200

    add some logging, improve requests making, improve df

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import argparse
+import logging
 
 def main():
     parser = argparse.ArgumentParser(description='url for a github repo.')
@@ -10,37 +11,47 @@ def main():
     # handy links for testing
     star_url =  "https://github.com/sunfounder/SunFounder_PiCar-V"
     starless_url = "https://github.com/SimeonKraev/UnInspired"
+    
+    base_url = f"https://api.github.com/repos/{user}/{repo}/stargazers"
     headers={"Accept": "application/vnd.github.v3.star+json"}
+    params = {"per_page": 100}
 
     url = "https://github.com/sunfounder/SunFounder_PiCar-V"#args.URL
 
     user = url.split("/")[-2]
     repo = url.split("/")[-1]
 
-    data = []
+    list_stargazers = []
     page = 1
-    while True:
-        url = f"https://api.github.com/repos/{user}/{repo}/stargazers?page={page}&per_page=100"
-        response = requests.get(url, headers=headers)
-        page_data = response.json()
-        if not page_data:
-            break
-        data.extend(page_data)
-        page += 1
-        print("page_data", page_data)
+    with requests.Session() as session:
+        session.headers.update(headers)
+
+        while True:
+            params['page'] = page 
+            response = session.get(base_url, params=params)
+
+            if response.status_code == 403 and 'rate limit' in response.text.lower():
+                logging.error("Rate limit reached. Please try again later.")
+                break
+            try:
+                page_data = response.json()
+                if not page_data:
+                    break
+                list_stargazers.extend(page_data)
+                page += 1
+            except ValueError as e:
+                logging.error(f"Failed because: {e}.")
+                exit(1)
 
 
     usr_info = ["starred_at", "login", "id", "node_id", "url", "followers_url", "subscriptions_url",
                      "organizations_url", "repos_url", "type", "site_admin"]
 
     data_rows = []
-    for starred_user in data:
+    for starred_user in list_stargazers:
         temp_dict = {"starred_at": starred_user.get("starred_at")}
         user_info = starred_user.get("user", {})
-        for key in usr_info:
-            if key in user_info:
-                temp_dict.update({key: user_info[key]})
-
+        temp_dict.update({key: value for key, value in user_info.items() if key in usr_info})
         data_rows.append(temp_dict)
 
     dataset = pd.DataFrame(data_rows)
