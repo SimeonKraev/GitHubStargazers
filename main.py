496f5d01aa6d5b95b3259c19562641a688b92e5e
SimeonKraev <shushumigaaa@gmail.com>
2024-06-25 10:52:35 +0200

    get multiple pages, header needed for star timestamp

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import requests
 
 star_url =  "https://github.com/sunfounder/SunFounder_PiCar-V"
 starless_url = "https://github.com/SimeonKraev/UnInspired"
+headers={"Accept": "application/vnd.github.v3.star+json"}
 
 url = star_url
 
 user = url.split("/")[-2]
 repo = url.split("/")[-1]
 
-url = f"https://api.github.com/repos/{user}/{repo}/stargazers"
-response = requests.get(url)
-page_data = response.json()
-
-print("page_data", page_data)
+page = 1
+while True:
+    url = f"https://api.github.com/repos/{user}/{repo}/stargazers?page={page}&per_page=100"
+    response = requests.get(url, headers=headers)
+    page_data = response.json()
+    if not page_data:
+        break
+    page += 1
+    print("page_data", page_data)
