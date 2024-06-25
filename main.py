fd3be2f83f3228bba989d5b004cefc90098d00e7
SimeonKraev <shushumigaaa@gmail.com>
2024-06-25 11:34:02 +0200

    extract stargazer info

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ url = star_url
 user = url.split("/")[-2]
 repo = url.split("/")[-1]
 
+data = []
 page = 1
 while True:
     url = f"https://api.github.com/repos/{user}/{repo}/stargazers?page={page}&per_page=100"
@@ -16,5 +17,21 @@ while True:
     page_data = response.json()
     if not page_data:
         break
+    data.extend(page_data)
     page += 1
     print("page_data", page_data)
+
+
+usr_info = ["starred_at", "login", "id", "node_id", "url", "followers_url", "subscriptions_url",
+                 "organizations_url", "repos_url", "type", "site_admin"]
+
+data_rows = []
+for starred_user in data:
+    temp_dict = {"starred_at": starred_user.get("starred_at")}
+    user_info = starred_user.get("user", {})
+    for key in usr_info:
+        if key in user_info:
+            temp_dict.update({key: user_info[key]})
+
+    data_rows.append(temp_dict)
+print(data_rows)
